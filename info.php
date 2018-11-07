e7af0b9886558df1a7e20a873c2d20b1fc36e731
babonza <damar.aji.s@gmail.com>
2018-11-08 02:35:46 +0700

    Update info.php

--- info.php
+++ info.php
@@ -1,24 +1,53 @@
-$wait = 1; // wait Timeout In Seconds
-$host = 'detik.com';
-$ports = [
-    'http'  => 80,
-    'https' => 443,
-    'ftp'   => 21,
-];
+<?php 
 
-foreach ($ports as $key => $port) {
-    $fp = @fsockopen($host, $port, $errCode, $errStr, $wait);
-    echo "Ping $host:$port ($key) ==> ";
-    if ($fp) {
-        echo 'SUCCESS';
-        fclose($fp);
-    } else {
-        echo "ERROR: $errCode - $errStr";
-    }
-    echo PHP_EOL;
+//Config information
+$email = "damar.santoso@metrodata.com";
+$server = "google.com"; //the address to test, without the "http://"
+$port = "80";
+
+
+//Create a text file to store the result of the ping for comparison
+$db = "pingdata.txt";
+
+if (file_exists($db)):
+    $previous_status = file_get_contents($db, true);
+else:
+    file_put_contents($db, "up");
+    $previous_status = "up";
+endif;
+
+//Ping the server and check if it's up
+$current_status =  ping($server, $port, 10);
+
+//If it's down, log it and/or email the owner
+if ($current_status == "down"):
+
+    echo "Server is down! ";
+    file_put_contents($db, "down");
+
+    if ($previous_status == "down"):
+        mail($email, "Server is down", "Your server is down.");
+        echo "Email sent.";     
+    endif;  
+
+else:
+
+    echo "Server is up! ";
+    file_put_contents($db, "up");
+
+    if ($previous_status == "down"):
+        mail($email, "Server is up", "Your server is back up.");
+        echo "Email sent.";
+    endif;
+
+endif;
+
+
+function ping($host, $port, $timeout)
+{ 
+  $tB = microtime(true); 
+  $fP = fSockOpen($host, $port, $errno, $errstr, $timeout); 
+  if (!$fP) { return "down"; } 
+  $tA = microtime(true); 
+  return round((($tA - $tB) * 1000), 0)." ms"; 
 }
-
-
-// Ping example.com:80 (http) ==> SUCCESS
-// Ping example.com:443 (https) ==> SUCCESS
-// Ping example.com:21 (ftp) ==> ERROR: 110 - Connection timed out
